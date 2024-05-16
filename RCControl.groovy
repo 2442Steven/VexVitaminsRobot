118458200bce2e4de9680ee75ea2067b4dc41234
2442Steven <stwang@bancroftschool.org>
2024-05-16 09:11:00 -0400

    Updated Control Script

--- RCControl.groovy
+++ RCControl.groovy
@@ -7,15 +7,12 @@ import com.neuronrobotics.sdk.addons.kinematics.MobileBase
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 import com.neuronrobotics.sdk.common.DeviceManager
 import com.neuronrobotics.sdk.util.ThreadUtil
-MobileBase base;
+
 //Check if the device already exists in the device Manager
-if(args==null){
-	base=DeviceManager.getSpecificDevice( "VexVitaminsRobot",{ScriptingEngine.gitScriptRun(	
+MobileBase base=DeviceManager.getSpecificDevice( "VexVitaminsRobot",{ScriptingEngine.gitScriptRun(	
 		"https://github.com/madhephaestus/VexVitaminsRobot.git",
 		"VexVitaminsRobot.xml", 
 		null )})
-}else
-	base=args.get(0)
 
 
 println "Now we will move just one leg"
@@ -23,6 +20,9 @@ DHParameterKinematics arm = base.getAppendages().get(0)
 double zLift=25
 println "Start from where the arm already is and move it from there with absolute location"
 TransformNR current = arm.getCurrentPoseTarget();
+
+TransformNR absolute = new TransformNR(400,0,5);
+
 current.translateZ(zLift);
-arm.setDesiredTaskSpaceTransform(current,  2.0);
+arm.setDesiredTaskSpaceTransform(absolute,  2.0);
 ThreadUtil.wait(2000)// wait for the arm to fully arrive
